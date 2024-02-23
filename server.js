d05b86b7a0f4fb42121a9ff8d7e690129ce8112c
Michael Idundun <122805291+We13b-MD@users.noreply.github.com>
2024-02-23 11:43:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
    res.setHeader("Access-Control-Allow-Origin", "https://servermikiflix-7.onrender.com");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
 res.setHeader("Cross-Origin-Opener-Policy", "cross-origin");
-
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 next();
 
  
