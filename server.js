944e368830ae88d11e3d0abf6989b20e79ba8128
Michael Idundun <122805291+We13b-MD@users.noreply.github.com>
2024-02-23 12:37:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(
 );
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "https://clientprimevideo-93e2aed43e2b.herokuapp.com");
-   res.setHeader("Access-Control-Allow-Origin", "https://servermikiflix-7.onrender.com");
+
+ 
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
 res.setHeader("Cross-Origin-Opener-Policy", "cross-origin");
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
