8155d9b8fa68a717475a9d42715639c33bb3e998
Michael Idundun <122805291+We13b-MD@users.noreply.github.com>
2024-02-23 11:07:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,8 @@ app.use(passport.initialize());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://clientprimevideo-93e2aed43e2b.herokuapp.com");
+   res.setHeader("Access-Control-Allow-Origin", "https://servermikiflix-7.onrender.com");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
 res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
 next();
